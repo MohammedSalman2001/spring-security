383f36b798b4f26008e6df4a47ef8e5011dd40bd
MohammedSalman2001 <salmannisthar123@gmail.com>
2023-10-11 03:28:38 +0530

    implement UserDetails interface

--- src/main/java/com/developerstack/security/api/ProductController.java
+++ src/main/java/com/developerstack/security/api/ProductController.java
@@ -1,31 +1,37 @@
 package com.developerstack.security.api;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/member/api/v1/products")
 public class ProductController {
     @PostMapping
+    @PreAuthorize("hasAuthority('product:write')")
     public String createProduct(){
         return "createProduct";
     }
 
     @PutMapping
+    @PreAuthorize("hasAuthority('product:write')")
     public String updateProduct(){
         return "updateProduct";
     }
 
     @DeleteMapping
+    @PreAuthorize("hasAuthority('product:write')")
     public String deleteProduct(){
         return "deleteProduct";
     }
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER','ROLE_USER')")
     public String findProduct(){
         return "findProduct";
     }
 
     @GetMapping( "list")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN','ROLE_MANAGER')")
     public String findAllProduct(){
         return "findAllProduct";
     }
--- src/main/java/com/developerstack/security/config/security/ApplicationSecurityConfig.java
+++ src/main/java/com/developerstack/security/config/security/ApplicationSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +20,7 @@ import static com.developerstack.security.config.permision.ApplicationUserRole.*
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -30,7 +32,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 authorizeHttpRequests()
                 .antMatchers("/","index").permitAll()
                 .antMatchers("/api/v1/**").hasRole(USER.name())
-             /*   .antMatchers(HttpMethod.GET,"/member/api/v1/**").hasAnyRole(ADMIN.name(),MANAGER.name())
+              /*  .antMatchers(HttpMethod.GET,"/member/api/v1/**").hasAnyRole(ADMIN.name(),MANAGER.name())
                 .antMatchers(HttpMethod.DELETE,"/member/api/v1/**").hasAuthority(PRODUCT_WRITE.getPermission())
                 .antMatchers(HttpMethod.PUT,"/member/api/v1/**").hasAuthority(PRODUCT_WRITE.getPermission())
                 .antMatchers(HttpMethod.POST,"/member/api/v1/**").hasAuthority(PRODUCT_WRITE.getPermission())*/
